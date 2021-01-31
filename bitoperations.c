2c3a84c03f3b411793006a504f625c6024f818b4
ShirinNagle <G00363458@gmit.ie>
2021-01-31 16:01:20 +0000

    Testing gcc works correctly

---
+++ bitoperations.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])//main takes 2 arguments, arg count and a pointer to a list
+{
+    //Print standard basic 1st program
+    printf("%s\n","Hello, world!");
+    //should always return 0 from main if there hasn't been an error, if not incl
+    //chaining programs on command line is not possible.
+    return 0;
+}
