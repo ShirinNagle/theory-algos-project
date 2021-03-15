9ad068a15fae064ba3f446370efe12e9f18dd587
ShirinNagle <G00363458@gmit.ie>
2021-03-15 16:03:51 +0000

    Added padding to preprocess

--- preprocess.c
+++ preprocess.c
@@ -9,7 +9,66 @@
 union Block{//all memory variables stored in same location
  BYTE bytes[64];
  WORD words[16];
+ uint64_t sixf[8];
 };
+
+enum Status{
+    READ, PAD, END
+};
+
+//loop through the preprocesses blocks
+int next_block(FILE *f, union Block *B, enum Status *S, uint64_t *nobits){ 
+    //no of bytes to read
+    size_t nobytes;
+
+    if(*S == END){
+        return 0;
+    } else if(*S == READ){
+        //Try to read 64 bytes
+        nobytes = fread(B->bytes, 1, 64, f);//B is now a pointer to a union block
+        //Calculate the total bits read so far.
+        *nobits = *nobits + (8 * nobytes);
+        //512 bits -> 512 - 65 same as 64 bytes -> 55 bytes
+        //enough room for padding
+        if(nobytes == 64){
+            return 1;
+        } else if(nobytes <= 55){
+            //Append a 1 bit(and seven 0 bits to make full byte)
+            B->bytes[nobytes++] = 0x80;//In bits: 1000000
+            //Append enough 0 bits, leaving 64 at the end
+            while(nobytes++ < 56){
+                B->bytes[nobytes]= 0x00;//in bits: 0000000
+            }
+            //Append length of original input (CHECK ENDIANESS)
+            B->sixf[7] = *nobits;
+           //say this is the last block
+           *S = END;
+        } else{
+            //Gotten to end of input message  - not em=nough room in this block for all the padding
+            //Append a 1 bit(and 7 0 bits to make a full byte).
+            B->bytes[nobytes] = 0x80;
+            //Append 0 bits.
+            while(nobytes++ < 64){
+                B->bytes[nobytes]= 0x00;//in bits: 0000000
+            }
+            // Change to staus to PAD
+            *S = PAD;
+        }
+    } else if(*S == PAD){
+        nobytes = 0;
+        //Append 0 bits
+        while(nobytes++ <56){
+                B->bytes[nobytes]= 0x00;//in bits: 0000000
+            }
+        //Append nobits as an integer
+        B->sixf[7] = *nobits;
+        //Change the status to END
+        *S = END;
+
+
+    }
+    return 1;
+}
 
 //on cli man 3 fopen, 3 means give c function fopen. man = manual page. Q to get out of a man pag
 
@@ -22,37 +81,26 @@ int main(int argc, char *argv[]){
     
     //total number of bits read
     uint64_t nobits =0;
+
+    //current status of reading input
+    enum Status S = READ;
    
     //* = pointer, File pointer for reading
     FILE *f;  
     //open file from cli for reading
     f = fopen(argv[1], "r"); // path and mode
     //number of bytes to read
-    size_t nobytes;
-    //try to read bytes
-    nobytes = fread(B.bytes,1, 64, f);//&b = memory address of b, read 1 byte, read 64 times, read from f
-    printf("Read %d bytes. \n", nobytes);
-    //update of bits read
-    nobits = nobits +(8 * nobytes);
-    //Print 16 x 32bit words to screen
-    for(int i=0; i < 16; i++){
-        printf("%08" PF " ", B.words[i]);
-        if(i+1 % 8 == 0)
-         printf("\n");
-    }
+    
 
-    while (!feof(f))
+    //loop through the preprocesses blocks
+    while (next_block(f, &B, &S, &nobits))
     {
-        //do same here
-        nobytes = fread(&B.bytes,1, 64, f);
-        printf("Read %d bytes: \n", nobytes);
-        nobits = nobits +(8 * nobytes);
-
+        
         for(int i=0; i < 16; i++){
         printf("%08" PF " ", B.words[i]);
-        if(i+1 % 8 == 0)
+        }
          printf("\n");
-    }
+    
     }
     fclose(f);//close file when finished
     printf("Total bits read %d. \n", nobits);
